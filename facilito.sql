3c7a189f628e766230e0513cfe5b30433ba4a616
Ander Guevara <anderguevaralaw@gmail.com>
2017-02-08 17:32:43 -0400

    Add files via upload

---
+++ facilito.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-02-2017 a las 21:32:24
+-- Versión del servidor: 5.7.14
+-- Versión de PHP: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `facilito`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `countries`
+--
+
+CREATE TABLE `countries` (
+  `id` int(10) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `countries`
+--
+
+INSERT INTO `countries` (`id`, `name`, `status`) VALUES
+(1, 'Venezuela', 1),
+(2, 'Colombia', 1),
+(3, 'Mexico', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`id`, `username`, `password`, `email`) VALUES
+(1, 'test1', 'pass1', 'test1@example.com'),
+(2, 'test2', 'pass2', 'test2@example.com'),
+(3, 'test3', 'pass3', 'test3@example.com'),
+(4, 'test4', 'pass4', 'test4@example.com'),
+(5, 'test5', 'pass5', 'test5@example.com'),
+(6, 'test6', 'pass6', 'test6@example.com'),
+(7, 'test7', 'pass7', 'test7@example.com'),
+(8, 'test8', 'pass8', 'test8@example.com'),
+(9, 'test9', 'pass9', 'test9@example.com'),
+(10, 'test10', 'pass10', 'test10@example.com'),
+(11, 'test11', 'pass11', 'test11@example.com'),
+(12, 'test12', 'pass12', 'test12@example.com'),
+(13, 'test13', 'pass13', 'test13@example.com'),
+(14, 'test14', 'pass14', 'test14@example.com'),
+(15, 'test15', 'pass15', 'test15@example.com'),
+(16, 'test16', 'pass16', 'test16@example.com'),
+(17, 'test17', 'pass17', 'test17@example.com'),
+(18, 'test18', 'pass18', 'test18@example.com'),
+(19, 'test19', 'pass19', 'test19@example.com'),
+(20, 'test20', 'pass20', 'test20@example.com'),
+(21, 'test21', 'pass21', 'test21@example.com'),
+(22, 'Codexpress', '123456', 'codexpress@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `countries`
+--
+ALTER TABLE `countries`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `countries`
+--
+ALTER TABLE `countries`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
